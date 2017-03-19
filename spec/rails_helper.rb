5ff9042a3f533abaa4266a5eb03b51a08db688fe
mendelB <mendelblesofsky@gmail.com>
2017-03-19 16:13:49 -0400

    add cofiguration for request helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -30,7 +30,7 @@ Shoulda::Matchers.configure do |config|
   end
 end
 
-
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 # Checks for pending migration and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
@@ -39,6 +39,7 @@ RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
+  config.include RequestSpecHelper, type: :request
   config.include FactoryGirl::Syntax::Methods
 
   config.before(:suite) do
